612ed1a157437d9acd2a1f54de6075e4b1ea8b4f
yannick-ipedis <yannick@ipedis.com>
2022-10-24 14:59:30 +0200

    Unaccessible carrousel

--- Caroussel/script.js
+++ Caroussel/script.js
@@ -8,9 +8,6 @@ $(document).on('ready', function() {
 
     $('.slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
         resetDots();
-        window.setTimeout(function(){
-            $(".slider .slide-content:eq("+ currentSlide +")").find("a").focus(); //Set link of new slide as next element in tab order
-        },500);
         var dot = $("li:eq("+ currentSlide +") .bt-dot"); //Get dot position of new slide
         setDotActive(dot);
     });
@@ -20,20 +17,15 @@ $(document).on('ready', function() {
         var index = $(this).parent().index(); //Get index of dot in list of navigation
         $(".slider").slick("slickGoTo", index); //Animate carrousel to selected slide
         setDotActive($(this));
-        window.setTimeout(function(){
-            $(".status").html("Sélectionné"); //Add sleect state manually for screen readers
-        },500)
     });
 
     // Adding select state to active navigation dot
     function setDotActive(dot){
-        dot.append('<span class="sr-only-active"> Sélectionné</span>').addClass("active");
+        dot.addClass("active");
     }
 
     // Reset values of navigation dots
     function resetDots(){
         $(".bt-dot").removeClass("active");
-        $(".sr-only-active").remove();
-        $(".status").html("");
     }
 });
