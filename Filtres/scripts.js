a111f21bdc3d72d947c33ef8cab3c39137d6c884
yannick-ipedis <yannick@ipedis.com>
2022-10-24 09:01:30 +0200

    Carrousel

--- Filtres/scripts.js
+++ Filtres/scripts.js
@@ -7,8 +7,6 @@ filters.forEach(filter => {
 
   filter.addEventListener('click', function() {
 
-    document.querySelectorAll('.filter').forEach(b=>b.setAttribute('aria-checked',false));
-
     let selectedFilter = filter.getAttribute('data-filter');
     let itemsToHide = document.querySelectorAll(`.projects .project:not([data-filter='${selectedFilter}'])`);
     let itemsToShow = document.querySelectorAll(`.projects [data-filter='${selectedFilter}']`);
@@ -27,8 +25,6 @@ filters.forEach(filter => {
       el.classList.remove('hide');
       el.classList.add('show'); 
     });
-
-    filter.setAttribute('aria-checked',true);
 
     document.getElementById('results').innerHTML = itemsToShow.length + text;
 
